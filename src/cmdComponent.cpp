4ff1c5bbbb1375ffbee9c9992f22e9a2e50701e8
Randy Yamanaka <ryama008@ucr.edu>
2017-02-18 01:13:09 +0000

    fourth commit

--- src/cmdComponent.cpp
+++ src/cmdComponent.cpp
@@ -4,12 +4,12 @@
         return (keyword.find_first_of(" \&;")==-1);
     }
     void parse(){
-        
+        return keyword;
     }
     bool is_connector(){
         if(keyword=="||" || keyword=="&&" || keyword == ";") return true;
         else return false;
     }
     string getWord(){
-        
+        return keyword;
     }
